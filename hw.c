4ad265f924a6c68d9f670dacbef7a175602d750c
dummy <dummy@github.com>
2016-04-19 12:51:59 +0200

    Fixed compile issue

--- hw.c
+++ hw.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-#include <avg.h>
+#include "avg.h"
 
 int main() {
 	printf("Hello welt!\n");
 	return (0);
 	
-	float a[] = { 12.0, 247.0, 348.0, 482.0, 237.0, 6835.0, 657.0,457.0, 48.0, 475.0, 7.0 }
+	float a[] = { 12.0, 247.0, 348.0, 482.0, 237.0, 6835.0, 657.0,457.0, 48.0, 475.0, 7.0 };
 	float b[10];
 	
 	average(a,b,10);
