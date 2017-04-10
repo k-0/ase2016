a5de183c18f243eadb60733fb714ec8c520ee3bb
dummy <dummy@github.com>
2017-04-10 14:39:23 +0200

    New Text

--- hw.c
+++ hw.c
@@ -3,7 +3,7 @@
 #include "avg.h"
 
 int main() {
-	printf("Hello World!\n");
+	printf("Hello Solar Systems!\n");
 	return (0);
 	
 	float a[] = { 12.0, 247.0, 348.0, 482.0, 237.0, 6835.0, 657.0,457.0, 48.0, 475.0, 7.0 };
