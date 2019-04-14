7b1d070d851301a5084e0f39986fe5811e239958
ase-2017 <danilo.beuche@gmail.com>
2019-04-14 15:07:58 +0000

    Translated to German

--- hw.c
+++ hw.c
@@ -3,11 +3,11 @@
 #include "avg.h"
 
 int main() {
-	printf("Hello Solar Systems!\n");
+	printf("Hallo Solar Systems!\n");
 	return (0);
-	
+
 	float a[] = { 12.0, 247.0, 348.0, 482.0, 237.0, 6835.0, 657.0,457.0, 48.0, 475.0, 7.0 };
 	float b[10];
-	
+
 	average(a,b,10);
 }
