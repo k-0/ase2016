96b189e447b76df849410dfd42e658869e4f6519
ase-2017 <danilo.beuche@gmail.com>
2019-06-21 13:14:44 +0000

    Broken or not?

--- hw.c
+++ hw.c
@@ -3,8 +3,8 @@
 #include "avg.h"
 
 int main() {
-	printf("Hallo Solar Systeme!\n");
-	return (0);
+	printf("allo Solar Systeme!\n");
+	return (1);
 
 	float a[] = { 12.0, 247.0, 348.0, 482.0, 237.0, 6835.0, 657.0,457.0, 48.0, 475.0, 7.0 };
 	float b[10];
